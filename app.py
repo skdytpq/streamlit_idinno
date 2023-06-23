4dc5a206d1651d145ba8acc89b64994940b7d943
joseph <skdytpq98@naver.com>
2023-06-23 18:35:37 +0900

    f

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+st.markdown('<style type="text/css">body { margin: 0; padding: 0; }</style>', unsafe_allow_html=True)
+st.markdown('<style>' + open('style.css').read() + '</style>', unsafe_allow_html=True)
 
 st.title('아이노유 서비스 크롤링 프로그램입니다.')
 option = st.selectbox('데이터 크롤링 타입 선택',
@@ -8,5 +10,5 @@ values = st.slider('크롤링 데이터 규모', 0.0, 100000.0, (5000.0,100000.0
 st.write('Values:', values)
 
 st.write('You selected:', option)
-st.markdown('<link href="styles.css" rel="stylesheet">', unsafe_allow_html=True)
+
 csv_file = st.file_uploader('크롤링 데이터 URL csv 를 업로드 해주세요', type=['csv'])
