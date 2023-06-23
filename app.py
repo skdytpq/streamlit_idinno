04801d5994d8c0476935333bb793fdd5e7edd055
joseph <skdytpq98@naver.com>
2023-06-23 18:21:10 +0900

    f

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title('아이노유 서비스 크롤링 프로그램입니다.')
+option = st.selectbox('데이터 크롤링 타입 선택',
+                    ('아이노유 페르소나', '병원 페르소나', '패션 데이터 페르소나'))
+values = st.slider('크롤링 데이터 규모', 0.0, 100000.0, (5000.0,100000.0))
+
+st.write('Values:', values)
+
+st.write('You selected:', option)
+
+csv_file = st.file_uploader('크롤링 데이터 URL csv 를 업로드 해주세요', type=['csv'])
