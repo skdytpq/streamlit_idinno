4b053b3b453b1700e5dd9b4a439dcec13d4457fd
Joseph Na <74703237+skdytpq@users.noreply.github.com>
2023-07-10 16:02:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 import streamlit as st
 
-# 커스텀 CSS를 사용하여 이미지를 특정 요소에 추가
+import streamlit as st
+
+# 커스텀 CSS를 사용하여 이미지를 대체할 요소에 추가
 st.markdown(
     """
     <style>
-    #root > div:nth-child(1) > div.withScreencast > div > div > header > div.css-14xtw13.e13qjvis0 > div:nth-child(3) > button > div > div {
+    #root > div:nth-child(1) > div.withScreencast > div > div > header > div.css-14xtw13.e13qjvis0 > div:nth-child(1),
+    #root > div:nth-child(1) > div.withScreencast > div > div > header > div.css-14xtw13.e13qjvis0 > div:nth-child(2),
+    #root > div:nth-child(1) > div.withScreencast > div > div > header > div.css-14xtw13.e13qjvis0 > div:nth-child(3) {
         background-image: url('https://ethno-mining.com/resources/iknowyou/image/code/K01.png');
         background-size: cover;
         background-position: center;
-        width: 10px;
-        height: 10px;
+        width: 20px;
+        height: 20px;
     }
     </style>
     """,
@@ -24,4 +28,4 @@ values = st.slider('크롤링 데이터 규모', 0.0, 100000.0, (5000.0, 100000.
 st.write('Values:', values)
 st.write('You selected:', option)
 
-csv_file = st.file_uploader('크롤링 데이터 URL csv를 업로드 해주세요', type=['csv'])
+csv_file = st.file_uploader('크롤링 데이터 URL csv를 
