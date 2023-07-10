43847a629920b7be2466afc5c3f63573165de551
Joseph Na <74703237+skdytpq@users.noreply.github.com>
2023-07-10 15:57:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,22 @@
 import streamlit as st
 
-# 커스텀 CSS를 사용하여 이미지를 툴바에 추가
+# 커스텀 CSS를 사용하여 이미지를 특정 요소에 추가
 st.markdown(
     """
     <style>
-    .toolbar {
-        display: flex;
-        align-items: center;
-        padding: 10px;
-    }
-    
-    .toolbar-image {
-        margin-right: 10px;
-    }
-    
-    .header-container {
-        display: flex;
-        align-items: center;
+    #root > div:nth-child(1) > div.withScreencast > div > div > header {
+        background-image: url('https://ethno-mining.com/resources/iknowyou/image/code/K01.png');
+        background-size: cover;
+        background-position: center;
         padding: 20px;
-        background-color: #f0f0f0;
-    }
-    
-    .header-title {
-        font-size: 24px;
-        margin-left: 10px;
     }
     </style>
     """,
     unsafe_allow_html=True
 )
 
-# 헤더 컨테이너 생성
-header_container = st.container()
-with header_container:
-    # 헤더 이미지 및 제목
-    st.markdown(
-        """
-        <div class="header-container">
-            <img class="toolbar-image" src="https://ethno-mining.com/resources/iknowyou/image/code/K01.png">
-            <h1 class="header-title">아이노유 서비스 크롤링 프로그램입니다.</h1>
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
+st.title('아이노유 서비스 크롤링 프로그램입니다.')
 
-# 데이터 선택 및 업로드
 option = st.selectbox('데이터 크롤링 타입 선택', ('아이노유 페르소나', '병원 페르소나', '패션 데이터 페르소나'))
 values = st.slider('크롤링 데이터 규모', 0.0, 100000.0, (5000.0, 100000.0))
 
