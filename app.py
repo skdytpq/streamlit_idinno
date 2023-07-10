f69592fdf284f44c8a572d451d9a574f1f0810a8
Joseph Na <74703237+skdytpq@users.noreply.github.com>
2023-07-10 15:44:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import streamlit as st
 
-# 이미지를 페이지 좌측 상단에 위치시키기 위한 CSS 스타일
+# 커스텀 CSS를 사용하여 이미지를 툴바에 추가
 st.markdown(
     """
     <style>
-    .image-container {
-        position: absolute;
-        top: 0;
-        left: 0;
+    .toolbar {
+        display: flex;
+        align-items: center;
         padding: 10px;
+    }
+    
+    .toolbar-image {
+        margin-right: 10px;
     }
     </style>
     """,
@@ -22,6 +25,19 @@ values = st.slider('크롤링 데이터 규모', 0.0, 100000.0, (5000.0, 100000.
 
 st.write('Values:', values)
 st.write('You selected:', option)
+
+csv_file = st.file_uploader('크롤링 데이터 URL csv를 업로드 해주세요', type=['csv'])
+
+# 툴바에 이미지 추가
+st.markdown(
+    """
+    <div class="toolbar">
+        <img class="toolbar-image" src="https://ethno-mining.com/resources/iknowyou/image/code/K01.png">
+        <span>툴바에 추가된 이미지입니다.</span>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
 csv_file = st.file_uploader('크롤링 데이터 URL csv를 업로드 해주세요', type=['csv'])
 
